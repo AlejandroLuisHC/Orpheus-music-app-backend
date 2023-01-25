44b670ab05762e3c902b72689cd15374c3bdd98b
Jose-Morcillo-Cascales <morcillopepe1@gmail.com>
2023-01-25 19:53:22 +0100

    cloudinary albums

--- src/controllers/albums.controller.js
+++ src/controllers/albums.controller.js
@@ -56,11 +56,9 @@ const albumController = {
     },
 
     postAlbum: async (req, res) => {
-        const { body, params: { id } } = req
+        const { body,files } = req
 
-        if (!body.img) {
-            body.img = "http://icons.iconseeker.com/png/fullsize/music-notes/note-yellow.png"
-        }
+       
         
         try {
             const albumExists = await Album.findOne({ name: body.name, ownership: body.ownership[0] })
@@ -70,22 +68,28 @@ const albumController = {
                     massage: "Album already exist"
                 })
             }
-            if (body.ownership) {
-                console.log(id, typeof body.ownership)
-
-                const album = await Album.create({ ...body })
+           
+            if(files?.image){
+                const { public_id, secure_url } = await uploadImage(files.image.tempFilePath)
+                await fs.unlink(files.image.tempFilePath)
+                
+                const album = await Album.create(
+                    { ...body,
+                        img: { id: public_id, url: secure_url }
+                    }
+                )
                 res.status(201).send({
                     status: "Album created collab 2",
                     data: album
                 })
-            } else {
-                console.log('1 collab')
-                const album = await Album.create({ ...body })
+            }else {
+                const album = await Album.create({ ...body})
                 res.status(201).send({
-                    status: "Album created 1",
+                    status: "Album created collab 2",
                     data: album
                 })
             }
+            
 
         } catch (error) {
             res.status(400).send(error.message)
@@ -104,6 +108,10 @@ const albumController = {
         try {
             const album = await Album.findByIdAndDelete(id)
 
+            if (album.img?.id) {
+                await destroyImage(playlist.img.id)
+            }
+
             if (!album) {
                 res.status(404).send({
                     status: "FALSE",
@@ -121,7 +129,7 @@ const albumController = {
 
     },
     patchAlbum: async (req, res) => {
-        const { params: { id }, body } = req
+        const { params: { id }, body, files } = req
 
         if (!mongoose.Types.ObjectId.isValid(id)) {
             return res.status(404).send({
@@ -131,23 +139,55 @@ const albumController = {
         }
 
         try {
-            const album = await Album.findByIdAndUpdate(
-                { _id: id },
-                { ...body }
-            )
+            if(files?.image){
+                
+                const albumFind = await Album.findById(id)
+               
+                if (albumFind.img?.id) {
 
-            if (!album) {
-                res.status(404).send({
-                    status: "FALSE",
-                    message: `Album ${id} was not found`
+                    await destroyImage(albumFind.img.id)
+                }
+                const { public_id, secure_url } = await uploadImage(files.image.tempFilePath)
+
+                await fs.unlink(files.image.tempFilePath)
+
+                const album = await Album.findByIdAndUpdate(
+                    { _id: id },
+                    {
+                        ...body,
+                        img: { id: public_id, url: secure_url }
+                    }
+                    )
+                if (!album) {
+                    res.status(404).send({
+                        status: "FALSE",
+                        message: `Album ${id} was not found`
+                    })
+    
+                }
+                res.status(201).send({
+                    status: "OK",
+                    message: `Album ${id} updated successfully`
                 })
-
+            }else{
+                const album = await Album.findByIdAndUpdate(
+                    { _id: id },
+                    {
+                        ...body,
+                    }
+                    )
+                if (!album) {
+                    res.status(404).send({
+                        status: "FALSE",
+                        message: `Album ${id} was not found`
+                    })
+    
+                }
+                res.status(201).send({
+                    status: "OK",
+                    message: `Album ${id} updated successfully`
+                })
             }
-            res.status(201).send({
-                status: "OK",
-                message: `Album ${id} updated successfully`
-            })
-
         } catch (error) {
             res.status(400).send(error.message)
 
